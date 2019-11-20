26b752c5905e12f70583c772d901e13c641e0273
gboriss <galinaborissenko@gmail.com>
2019-11-20 23:35:34 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 
+let widget = document.querySelector('#widget')
+
 let apiKey = '9a3f56db816fe284dd580a82176924f9'
 let q = 'Moscow,ru'
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${q}&APPID=${apiKey}`
@@ -7,4 +9,7 @@ let form = document.getElementById('search-form')
 form.onsubmit = e => e.preventDefault()
 
 
+
+hearts.classList.add('#widget')
+// hearts.classList.remove('#widget')
 
